7a03a0b0757e350a66cb2a95721d65df51651469
刘敬雄 <jingxiong.liu@esaty.com>
2018-04-27 17:50:42 +0800

    测试交互，解决跨域

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -2,21 +2,18 @@
 /**
  * Created by PhpStorm.
  * User: liujingxiong
+ * Content: 登录控制器
  * Date: 2018/4/27
- * Time: 下午2:40
+ * Time: 下午5:30
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Order extends MY_Controller
+class Login extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
-    }
-
-    public function index()
-    {
-        $data = $this->readdb->select('*')->from('t_adminer_0')->get()->result_array();
-        echo json_encode($data);
+        $this->load->database();
+        $this->readdb = $this->load->database('readdb', TRUE);
     }
 }
