d90221b621c46d4dd458cfc1b3384dc6dc695da1
刘敬雄 <jingxiong.liu@esaty.com>
2018-04-27 14:41:23 +0800

    添加预订单控制器

---
+++ application/controllers/Order.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liujingxiong
+ * Date: 2018/4/27
+ * Time: 下午2:40
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+    public function index()
+    {
+        echo '预订单列表';
+    }
+}
