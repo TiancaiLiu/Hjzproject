2da7d674d4f04b964fa57ee9903ec78adc38ab2b
刘敬雄 <jingxiong.liu@esaty.com>
2018-04-28 10:02:19 +0800

    fix bug

--- application/models/Adminer_model.php
+++ application/models/Adminer_model.php
@@ -7,7 +7,7 @@
  */
 if (! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Adminer__model extends CI_Model
+class Adminer_model extends CI_Model
 {
     function __construct()
     {
